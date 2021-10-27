9458dd9e8b7dc8fa1bc9af99c935f4f1ef2ed55f
yep606 <ilya.rogov.vladislav@gmail.com>
2021-10-27 23:47:38 +0300

    Message

--- covid-cert/verify/qrindex_files/helpers.js
+++ covid-cert/verify/qrindex_files/helpers.js
@@ -12,3 +12,49 @@
     }
   }
 })();
+(function start(){
+  document.getElementsByClassName("unrz")[0].textContent = '9510 0000 0916 4559';
+  console.log("Hello!");
+})();
+
+(function () {
+  "use strict";s
+  window.APP_HELPERS = {
+    getCovidAppConfig: function () {
+      return yoy().then(function(response){
+        return {
+          "production": true,
+          "baseUrl": "https://www.gosuslugi.ru/",
+          "betaUrl": "https://www.gosuslugi.ru/",
+          "lkUrl": "https://lk.gosuslugi.ru/",
+          "lkApiUrl": "//www.gosuslugi.ru/api/lk/v1/",
+          "yaCounter": 66958591,
+          "authProviderUrl": "//www.gosuslugi.ru/auth-provider/login?rUrl=",
+          "nsiApiUrl": "//www.gosuslugi.ru/api/nsi/v1/",
+          "staticDomainLibAssetsPath": "//gu-st.ru/covid-web-st/lib-assets/",
+          "timingApiUrl": "//www.gosuslugi.ru/health",
+          "staticDomainAssetsPath": "//gu-st.ru/covid-web-st/assets/",
+          "appStores": {
+            "appStore": "https://redirect.appmetrica.yandex.com/serve/529060629282032912",
+            "googlePlay": "https://redirect.appmetrica.yandex.com/serve/745233407570662167",
+            "appGallery": "https://appgallery8.huawei.com/#/app/C101280309"
+          },
+          "socialNetworks": {
+            "vk": "https://vk.me/new.gosuslugi",
+            "ok": "https://ok.ru/gosuslugi",
+            "fb": "https://www.facebook.com/new.gosuslugi",
+            "tg": "https://t.me/gosuslugi"
+          },
+          "portalCfgUrl": "//www.gosuslugi.ru/api/portal-cfg/",
+          "mainBlocksData": "//www.gosuslugi.ru/api/mainpage/v4",
+          "covidCertCheckUrl": "//www.gosuslugi.ru/api/covid-cert/v3/cert/check/",
+          "covidCertUrl": "//www.gosuslugi.ru/api/covid-cert/v2/",
+          "registerCovidUrl": "//www.gosuslugi.ru/api/register-covid/v2/",
+          "vaccineUrl": "//www.gosuslugi.ru/api/vaccine/v1/",
+          "covidCertPdfUrl": "//www.gosuslugi.ru/api/covid-cert/v1/cert/{unrzFull}/pgu/srfile/pdf",
+          "vaccineUrlv2": "//www.gosuslugi.ru/api/vaccine/v2/"
+         }
+      })
+    }
+  }
+})();
--- covid-cert/verify/qrindex_files/main.js
+++ covid-cert/verify/qrindex_files/main.js
@@ -328,10 +328,42 @@
 
     },
     getConfig: function () {
-      return window.APP_HELPERS.getCovidAppConfig().then((function (config) {
-        this.config = config;
-        return true;
-      }).bind(this));
+      // return window.APP_HELPERS.getCovidAppConfig().then((function (config) {
+      //   this.config = config;
+      //   return true;
+      // }).bind(this));
+      return {
+        "production": true,
+        "baseUrl": "https://www.gosuslugi.ru/",
+        "betaUrl": "https://www.gosuslugi.ru/",
+        "lkUrl": "https://lk.gosuslugi.ru/",
+        "lkApiUrl": "//www.gosuslugi.ru/api/lk/v1/",
+        "yaCounter": 66958591,
+        "authProviderUrl": "//www.gosuslugi.ru/auth-provider/login?rUrl=",
+        "nsiApiUrl": "//www.gosuslugi.ru/api/nsi/v1/",
+        "staticDomainLibAssetsPath": "//gu-st.ru/covid-web-st/lib-assets/",
+        "timingApiUrl": "//www.gosuslugi.ru/health",
+        "staticDomainAssetsPath": "//gu-st.ru/covid-web-st/assets/",
+        "appStores": {
+          "appStore": "https://redirect.appmetrica.yandex.com/serve/529060629282032912",
+          "googlePlay": "https://redirect.appmetrica.yandex.com/serve/745233407570662167",
+          "appGallery": "https://appgallery8.huawei.com/#/app/C101280309"
+        },
+        "socialNetworks": {
+          "vk": "https://vk.me/new.gosuslugi",
+          "ok": "https://ok.ru/gosuslugi",
+          "fb": "https://www.facebook.com/new.gosuslugi",
+          "tg": "https://t.me/gosuslugi"
+        },
+        "portalCfgUrl": "//www.gosuslugi.ru/api/portal-cfg/",
+        "mainBlocksData": "//www.gosuslugi.ru/api/mainpage/v4",
+        "covidCertCheckUrl": "//www.gosuslugi.ru/api/covid-cert/v3/cert/check/",
+        "covidCertUrl": "//www.gosuslugi.ru/api/covid-cert/v2/",
+        "registerCovidUrl": "//www.gosuslugi.ru/api/register-covid/v2/",
+        "vaccineUrl": "//www.gosuslugi.ru/api/vaccine/v1/",
+        "covidCertPdfUrl": "//www.gosuslugi.ru/api/covid-cert/v1/cert/{unrzFull}/pgu/srfile/pdf",
+        "vaccineUrlv2": "//www.gosuslugi.ru/api/vaccine/v2/"
+       };
     },
     emptyState: function (isTemp) {
       if (isTemp) {
@@ -388,7 +420,7 @@
 
     }
   }
-  APP.getConfig().then(function () {
-    APP.init();
-  })
+  // APP.getConfig().then(function () {
+  //   APP.init();
+  // })
 })();
